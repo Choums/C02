1c99d3b75d1ce6c6ccd1cfd8f72cc73f19847cf3
Chams <chams.aidel@outlook.fr>
2020-07-06 19:43:38 +0200

    j11

---
+++ ex09/ft_strcapitalize.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcapitalize.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: caidel <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/05 18:08:37 by caidel            #+#    #+#             */
+/*   Updated: 2020/07/06 11:24:54 by caidel           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strlowcase(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		if ((str[i] >= 65 && str[i] <= 90))
+			str[i] += 32;
+		i++;
+	}
+	return (str);
+}
+
+char	*ft_strcapitalize(char *str)
+{
+	int i;
+
+	i = 0;
+	ft_strlowcase(str);
+	if (i == 0 && (str[i] >= 'a' && str[i] <= 'z'))
+		str[i] -= 32;
+	while (str[i])
+	{
+		if ((str[i] >= 'a' && str[i] <= 'z'))
+		{
+			if (!(str[i - 1] >= 'a' && str[i - 1] <= 'z'))
+			{
+				if (!(str[i - 1] >= 'A' && str[i - 1] <= 'Z'))
+				{
+					if (!(str[i - 1] >= '0' && str[i - 1] <= '9'))
+						str[i] -= 32;
+				}
+			}
+		}
+		i++;
+	}
+	return (str);
+}
---
+++ ex10/ft_strlcpy.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: caidel <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/06 11:26:24 by caidel            #+#    #+#             */
+/*   Updated: 2020/07/06 19:35:03 by caidel           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	s;
+
+	i = 0;
+	s = 0;
+	while (src[s])
+		s++;
+	while (src[i] && i < size - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (s);
+}
