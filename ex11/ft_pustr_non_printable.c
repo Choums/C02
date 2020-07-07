bf7693c5f0d1be5dab06b80910b3652b6339c8e8
Chams <chams.aidel@outlook.fr>
2020-07-07 13:02:36 +0200

    fin

--- ex11/ft_pustr_non_printable.c
+++ ex11/ft_pustr_non_printable.c
@@ -1,27 +1,52 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_str_is_printable.c                              :+:      :+:    :+:   */
+/*   ft_pustr_non_printable.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: caidel <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/07/05 16:57:39 by caidel            #+#    #+#             */
-/*   Updated: 2020/07/07 12:49:54 by caidel           ###   ########.fr       */
+/*   Created: 2020/07/06 14:35:11 by caidel            #+#    #+#             */
+/*   Updated: 2020/07/07 12:44:53 by caidel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_str_is_printable(char *str)
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_hexa(char c)
+{
+	char *hexa;
+	char h;
+
+	h = 0;
+	hexa = "0123456789abcdef";
+	h = c / 16;
+	ft_putchar(hexa[h]);
+	h = c % 16;
+	ft_putchar(hexa[h]);
+}
+
+void	ft_putstr_non_printable(char *str)
 {
 	int i;
+	int h;
 
 	i = 0;
-	if (str[i] == '\0')
-		return (1);
 	while (str[i])
 	{
-		if ((str[i] < 32) || (str[i] > 126))
-			return (0);
+		if ((str[i] >= 0 && str[i] <= 31) || (str[i] > 126))
+		{
+			ft_putchar('\\');
+			ft_hexa(str[i]);
+		}
+		else
+		{
+			ft_putchar(str[i]);
+		}
 		i++;
 	}
-	return (1);
 }
